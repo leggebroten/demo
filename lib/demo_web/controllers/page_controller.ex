9c4c4daf45daef834dcee4de3c975b61d81be1a4
Lee Eggebroten <lee@tripping.com>
2018-03-02 21:16:03 -0700

    Demonstrate basic thoughts for exceptions

--- lib/demo_web/controllers/page_controller.ex
+++ lib/demo_web/controllers/page_controller.ex
@@ -1,7 +1,18 @@
 defmodule DemoWeb.PageController do
   use DemoWeb, :controller
 
+  alias MyModule
+
   def index(conn, _params) do
-    render conn, "index.html"
+    MyModule.go()
+  rescue
+    e -> show_what_exception_should_look_like_when_caught_by_plug(conn, e)
   end
+
+  defp show_what_exception_should_look_like_when_caught_by_plug(conn, exception) do
+    conn
+    |> put_resp_header("content-type", "application/json; charset=utf-8")
+    |> send_resp(200, TrippingException.toJSON(exception))
+  end
+
 end
